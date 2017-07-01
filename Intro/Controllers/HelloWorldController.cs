a2e44e4dc2d011ca5fde0b1eafc01d224ece9d8e
Hsu Peng Jun <hsu.pengjun@outlook.com>
2017-07-01 19:39:50 +0800

    使用 Razor 產生 View。

--- Intro/Controllers/HelloWorldController.cs
+++ Intro/Controllers/HelloWorldController.cs
@@ -4,22 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Intro.Controllers
-{
-    public class HelloWorldController : Controller
-    {
-        public IActionResult Index()
-        {
-            return Json("你好！");
+namespace Intro.Controllers {
+    public class HelloWorldController : Controller {
+        public IActionResult Index(Int64 repeat = 5) {
+            ViewData["repeat"] = repeat;
+            return View();
         }
         // name 以及 friend 是 Query Parameter
         // 你也可以為他們提供 default
-        public IActionResult Welcome(String name, String friend)
-        {
+        public IActionResult Welcome(String name, String friend) {
             if (name == null) {
                 return NotFound();
             }
             return Json($"{name}，你好！" + ((friend == null) ? "你沒有朋友。" : $"你的朋友是{friend}。"));
+        }
+    }
+
+    public static class ExtensionMethods {
+        public static String Repeated(this String value, Int64 times) {
+            var result = "";
+            while (times -- != 0) {
+                result += value;
+            }
+            return result;
         }
     }
 }
